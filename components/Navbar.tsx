444bdc13246e14f239b69edb6f3c7c63794c2f49
PrasadSimhadri <saisimhadri2207@gmail.com>
2025-10-31 21:21:01 +0530

    Implemented authentication using Cleark Auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import NavItems from './NavItems'
 
 const Navbar = () => {
@@ -18,8 +18,18 @@ const Navbar = () => {
             </Link>
             <div className='flex items-center gap-8'>
                 <NavItems />
-            </div>
-        </nav>
+                <SignedOut>
+                        <SignInButton>
+                            <button className="btn-signin">
+                         Sign In
+                            </button>
+                        </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div >
+        </nav >
     )
 }
 
