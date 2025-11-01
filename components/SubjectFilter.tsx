8877289f64f8841de34252e1054d6c5cb896152e
PrasadSimhadri <saisimhadri2207@gmail.com>
2025-11-01 17:48:10 +0530

    implemented subject filtering option in companions library

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+'use client'
+import { subjects } from '@/constants'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils'
 
 const SubjectFilter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const query = searchParams.get('subject') || '';
+
+    const [subject, setSubject] = useState(query);
+
+    useEffect(() => {
+        let newUrl = "";
+        if (subject === "all") {
+            newUrl = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                keysToRemove: ['subject'],
+            });
+        } else {
+            newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: 'subject',
+                value: subject,
+            });
+        }
+        router.push(newUrl);
+    }, [subject]);
+    return (
+        <div>
+            <Select onValueChange={setSubject} value={subject}>
+                <SelectTrigger className="input capitalize">
+                    <SelectValue placeholder="Filter by Subject" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value='all'>All Subjects</SelectItem>
+                    {subjects.map((subject) => (
+                        <SelectItem
+                            key={subject}
+                            value={subject}
+                            className="capitalize"
+                        >
+                            {subject}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        </div>
+    )
 }
 
 export default SubjectFilter
